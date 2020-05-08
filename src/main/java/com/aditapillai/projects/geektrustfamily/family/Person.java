4b2ea9f56ed5a59d4f9803c91e9eb0ca84f1e42a
unsubd <aditapillai@gmail.com>
2020-05-08 08:58:12 +0530

    (fix): equals for Person

--- src/main/java/com/aditapillai/projects/geektrustfamily/family/Person.java
+++ src/main/java/com/aditapillai/projects/geektrustfamily/family/Person.java
@@ -55,7 +55,7 @@ public abstract class Person {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || Person.class == o.getClass()) return false;
+        if ((!(o instanceof Person))) return false;
         Person person = (Person) o;
         return name.equals(person.name);
     }
